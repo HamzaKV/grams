048ffaa308a77a9182b20d064d8d7ac40fa0ccf2
Hamza <hamzakv333@gmail.com>
2023-02-14 21:32:01 -0500

    fix subscriber key bug

--- src/hooks/use-store-node.ts
+++ src/hooks/use-store-node.ts
@@ -5,32 +5,34 @@ import { getter, setter } from '../utils/state-fns';
 import { GramNode } from '../types/gram';
 import { supportedStateTypes } from '../constants/strings';
 import type { StateKeys } from '../utils/create-store';
-import type { StateMap } from '../models/state-map';
 
 const useStoreNode = (
     key: string | ((stateKeys: StateKeys) => string)
 ): GramNode<unknown> | null | undefined => {
     const { map, stateKeys } = useAppContext();
 
+    const resolvedKey = useMemo(() => {
+        if (stateKeys) return typeof key === 'string' ? key : key(stateKeys);
+        return null;
+    }, [key, stateKeys]);
+
     const [, updateState] = useState(1);
 
+    const forceUpdate = () => {
+        updateState((prev) => (prev === 0 ? 1 : 0));
+    };
+
     const node = useMemo(() => {
-        return (map as StateMap).get(
-            typeof key === 'string' ? key : key(stateKeys as StateKeys)
-        );
-    }, [key, map, stateKeys]);
+        return map && resolvedKey ? map.get(resolvedKey) : null;
+    }, [map, resolvedKey]);
 
     useEffect(() => {
         let s: ReturnType<Subscriber> | undefined;
-        if (map && node && stateKeys) {
-            s = subscriber(
-                typeof key === 'string' ? key : key(stateKeys)
-            );
+        if (map && node && resolvedKey) {
+            s = subscriber(resolvedKey);
             if (node.stateType === supportedStateTypes.stateful) {
                 s.setId(node.subscribers.size);
-                node.subscribers.add(() => {
-                    updateState((prev) => (prev === 0 ? 1 : 0));
-                });
+                node.subscribers.add(forceUpdate);
             }
         }
 
@@ -48,7 +50,7 @@ const useStoreNode = (
                     );
             }
         };
-    }, [map, key, node, stateKeys]);
+    }, [map, node, resolvedKey]);
 
     return node;
 };
